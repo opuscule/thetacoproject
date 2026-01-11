93a728352bf0c2a7a3d6166e6666d87fdb558900
Robert C. Bowman III <rbowman@hanekedesign.com>
2026-01-11 16:58:27 -0500

    map pin, stamford, 404

---
+++ src/content/config.ts
@@ -0,0 +1,33 @@
+import { defineCollection, z } from 'astro:content';
+
+const reviews = defineCollection({
+  type: 'data',
+  schema: z.object({
+    id: z.number(),
+    created_at: z.string(),
+    display: z.boolean(),
+    name: z.string(),
+    quote: z.string(),
+    platform: z.string(),
+    link: z.string().url(),
+    location: z.string(),
+    type: z.enum(['Restaurant', 'Catering']),
+  }),
+});
+
+const press = defineCollection({
+  type: 'data',
+  schema: z.object({
+    id: z.number(),
+    title: z.string(),
+    description: z.string(),
+    publish_date: z.string(),
+    link: z.string().url(),
+    image: z.string(),
+  }),
+});
+
+export const collections = {
+  reviews,
+  press,
+};
