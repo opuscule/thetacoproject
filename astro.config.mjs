233198ef81c4f8c56c23d94d0cf77ddcfb3a1b0b
Robert C. Bowman III <rbowman@hanekedesign.com>
2026-02-13 12:53:39 -0500

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
+import sitemap from '@astrojs/sitemap';
 
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://thetacoproject.com',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react(), keystatic()],
+  integrations: [react(), keystatic(), sitemap()],
   adapter: netlify()
 });
